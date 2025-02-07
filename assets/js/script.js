85791e9dd2af7b0195987029e3104c79fd60467a
carloszamorano10 <ca.zamorano@hotmail.com>
2025-02-07 11:07:34 -0300

    avances js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("entrada");
+const btn = document.getElementById("boton");
+const contadorTareas = document.getElementById("tareasTotal");
+const contadorRealizadas = document.getElementById("tareasRealizadas");
+const dibujarTareas = document.querySelector("tbody");
+
+const tareas = [];
+
+btn.addEventListener("click", () => {
+  const tarea = { id: Date.now(), nombre: input.value, realizada: false };
+  tareas.push(tarea);
+  input.value = "";
+  let html = "";
+  for (const tarea of tareas) {
+    html += `
+        <tr>
+           <td>${tarea.id}</td>
+           <td>${tarea.nombre}</td>
+        </tr>
+        `;
+  }
+  dibujarTareas.innerHTML = html;
+});
+console.log(tareas);
