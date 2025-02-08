c0a9dfb0bdfddd47d56103640437cf390869339a
carloszamorano10 <ca.zamorano@hotmail.com>
2025-02-08 00:20:52 -0300

    avances

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,49 @@ const contadorTareas = document.getElementById("tareasTotal");
 const contadorRealizadas = document.getElementById("tareasRealizadas");
 const dibujarTareas = document.querySelector("tbody");
 
+
 const tareas = [];
+let contador = 0
 
 btn.addEventListener("click", () => {
   const tarea = { id: Date.now(), nombre: input.value, realizada: false };
   tareas.push(tarea);
   input.value = "";
+  renderizar()
+});
+
+
+const borrar = (id) =>{
+  const index = tareas.findIndex((ele) => ele.id === id)
+  tareas.splice(index,1)
+  renderizar()
+}
+
+
+const renderizar = ()=>{
   let html = "";
   for (const tarea of tareas) {
     html += `
         <tr>
            <td>${tarea.id}</td>
            <td>${tarea.nombre}</td>
+           <td><input type="checkbox" onclick="realizadas()" id="check"/></td>
+           <td><i class="fa-solid fa-circle-minus eliminar" onclick="borrar(${tarea.id})"></i></td>
         </tr>
         `;
   }
+   
   dibujarTareas.innerHTML = html;
   contadorTareas.innerHTML = tareas.length
-});
-console.log(tareas);
+}
+
+
+const realizadas = ()=>{
+  const realizado = document.getElementById("check")
+
+  if(realizado.checked === true){
+     tareas.realizada = true
+  } else if (realizado.checked === false){
+    tareas.realizada = false
+  }  
+}
